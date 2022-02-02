75852434563bb74a6dc6e4b75dd90836a2904163
Kazato Sugimoto <uiureo@gmail.com>
2022-02-02 13:49:51 +0900

    Add tests

--- tests/test.rs
+++ tests/test.rs
@@ -4,6 +4,10 @@ use thumbtaro::{hello, thumb, original};
 use actix_web::{test, App, web};
 use dotenv;
 
+fn setup() {
+  dotenv::from_filename(".env.test").ok();
+}
+
 fn config(cfg: &mut web::ServiceConfig) {
   cfg.service(
       web::scope("")
@@ -14,21 +18,8 @@ fn config(cfg: &mut web::ServiceConfig) {
 }
 
 #[actix_rt::test]
-async fn test_hello() {
-  let mut app = test::init_service(App::new().configure(config)).await;
-
-  let req = test::TestRequest::with_uri("/").to_request();
-  let resp = test::call_service(&mut app, req).await;
-
-  assert!(resp.status().is_success());
-
-  let body = test::read_body(resp).await;
-  assert_eq!(std::str::from_utf8(body.as_ref()).unwrap(), "hello");
-}
-
-#[actix_rt::test]
 async fn test_original_not_found() {
-  dotenv::from_filename(".env.test").ok();
+  setup();
   let mut app = test::init_service(App::new().configure(config)).await;
 
   let req = test::TestRequest::with_uri("/orig/not_found").to_request();
@@ -36,4 +27,30 @@ async fn test_original_not_found() {
   let status = resp.status();
 
   assert_eq!(status.as_str(), "404");
+}
+
+const IMAGE_PATH: &str = "food/banana.png";
+
+#[actix_rt::test]
+async fn test_original_found() {
+  setup();
+  let mut app = test::init_service(App::new().configure(config)).await;
+
+  let req = test::TestRequest::with_uri(&format!("/orig/{}", IMAGE_PATH)).to_request();
+  let resp = test::call_service(&mut app, req).await;
+  let status = resp.status();
+
+  assert_eq!(status.as_str(), "200");
+}
+
+#[actix_rt::test]
+async fn test_thumb_found() {
+  setup();
+  let mut app = test::init_service(App::new().configure(config)).await;
+
+  let req = test::TestRequest::with_uri(&format!("/thumb/400x400/{}", IMAGE_PATH)).to_request();
+  let resp = test::call_service(&mut app, req).await;
+  let status = resp.status();
+
+  assert_eq!(status.as_str(), "200");
 }
